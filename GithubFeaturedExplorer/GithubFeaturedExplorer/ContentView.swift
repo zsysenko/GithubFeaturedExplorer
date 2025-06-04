0998c365edbb9461cd0ea666987c621c4ed2f6a1
James Lunch <zsysenko@gmail.com>
2025-06-04 14:35:44 +0200

    Base project structure.

---
+++ GithubFeaturedExplorer/GithubFeaturedExplorer/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  GithubFeaturedExplorer
+//
+//  Created by EVGENY SYSENKA on 04/06/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    ContentView()
+}
