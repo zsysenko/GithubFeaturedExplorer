7086164233b787b15c7a9a1b3b4134c3f29352b8
James Lunch <zsysenko@gmail.com>
2025-06-05 18:29:56 +0200

    Add settings to change appearance  mode.

--- GithubFeaturedExplorer/GithubFeaturedExplorer/GithubFeaturedExplorerApp.swift
+++ GithubFeaturedExplorer/GithubFeaturedExplorer/GithubFeaturedExplorerApp.swift
@@ -8,30 +8,65 @@
 import SwiftUI
 import SwiftData
 
+enum NavigationRoute: Hashable {
+    case featuredRepoList
+    case repoDetail(repository: Repository)
+}
+
 @main
 struct GithubFeaturedExplorerApp: App {
-//    var sharedModelContainer: ModelContainer = {
-//        let schema = Schema([
-//            
-//        ])
-//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-//
-//        do {
-//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-//        } catch {
-//            fatalError("Could not create ModelContainer: \(error)")
-//        }
-//    }()
+    @AppStorage("appearance") private var appearance: Appearance = .unspecified
     
-    private var paths: [NavigationPath] = []
+    @State private var navigationRoutes: [NavigationRoute] = []
 
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                FeaturedList()
+            NavigationStack(path: $navigationRoutes) {
+                FeaturedListScreen()
+                    .navigationDestination(for: NavigationRoute.self) { route in
+                        screenForRoute(route)
+                    }
+            }
+            .onNavigate { navigationType in
+                perfomNavigation(for: navigationType)
+            }
+            .onAppear {
+                let window = UIApplication.shared
+                    .connectedScenes
+                    .compactMap { $0 as? UIWindowScene }
+                    .flatMap { $0.windows }
+                    .first
+                
+                window?.overrideUserInterfaceStyle = appearance.userInterfaceStyle
             }
         }
         .environment(FeaturedListModel(apiService: ApiService()))
-//        .modelContainer(sharedModelContainer)
+    }
+    
+    @ViewBuilder
+    private func screenForRoute( _ route: NavigationRoute) -> some View {
+        switch route {
+            case .featuredRepoList:
+                FeaturedListScreen()
+                
+            case .repoDetail(let repository):
+                RepositoryDetailScreen(repository: repository)
+        }
+    }
+    
+    private func perfomNavigation(for navigationType: NavigationType) {
+        switch navigationType {
+            case .push(let route):
+                navigationRoutes.append(route)
+                
+            case .pop(let route):
+                if route == .featuredRepoList {
+                    navigationRoutes = []
+                    
+                } else {
+                    guard let index = navigationRoutes.firstIndex(where: { $0 == route }) else { return }
+                    navigationRoutes = Array(navigationRoutes.prefix(upTo: index + 1))
+                }
+        }
     }
 }
--- GithubFeaturedExplorer/GithubFeaturedExplorer/UserInterface/Settings/SettingsScreen.swift
+++ GithubFeaturedExplorer/GithubFeaturedExplorer/UserInterface/Settings/SettingsScreen.swift
@@ -7,9 +7,53 @@
 
 import SwiftUI
 
+enum Appearance: String, CaseIterable, Identifiable {
+    case unspecified = "Unspecified"
+    case light = "Light"
+    case dark = "Dark"
+    
+    var id: String { self.rawValue }
+    
+    var userInterfaceStyle: UIUserInterfaceStyle {
+        switch self {
+            case .light:
+                return .light
+            case .dark:
+                return .dark
+            case .unspecified:
+                return .unspecified
+        }
+    }
+}
+
 struct SettingsScreen: View {
+    @AppStorage("appearance") private var selectedAppearance: Appearance = .unspecified
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Select Appearance")
+                .font(.headline)
+                .padding()
+            
+            Picker("Appearance", selection: $selectedAppearance) {
+                ForEach(Appearance.allCases) { mode in
+                    Text(mode.rawValue).tag(mode)
+                }
+            }
+            .pickerStyle(.segmented)
+            .padding([.leading, .trailing], 20)
+            
+            Spacer()
+        }
+        .onChange(of: selectedAppearance, initial: true, { _, newValue in
+            let window = UIApplication.shared
+                .connectedScenes
+                .compactMap { $0 as? UIWindowScene }
+                .flatMap { $0.windows }
+                .first
+            
+            window?.overrideUserInterfaceStyle = newValue.userInterfaceStyle
+        })
     }
 }
 
